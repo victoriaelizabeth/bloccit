608c846d52c2d3b77cbc0a5900a57f664345f049
Victoria Stubbolo <victoriaelizabeths@gmail.com>
2018-06-20 05:54:23 -0700

    assignment 16 update

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@ Post.find_or_create_by(title: "A unique title", body: "A unique body")
 puts "#{Post.count}"
 
 puts "#{Comment.count}"
-Comment.find_or_create_by(body: "A unique body")
+Comment.find_or_create_by(post: posts.first, body: "A unique body")
 puts "#{Comment.count}"
 
 puts "Seed finished"
