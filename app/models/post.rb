d754482ed4b5905208f7bc2be6cb2c75baed7f19
Victoria Stubbolo <victoriaelizabeths@gmail.com>
2018-10-04 23:17:41 -0700

    checkpoint 34 updated

--- app/models/post.rb
+++ app/models/post.rb
@@ -27,6 +27,6 @@ class Post < ActiveRecord::Base
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
-   end
-
+    end
+  end 
 end
