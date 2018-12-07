6a87db74ff504fedda2fef03375deef32e500836
Victoria Stubbolo <victoriaelizabeths@gmail.com>
2018-12-07 00:03:42 -0800

    assignment 35 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  after_create :create_favorite
 
   default_scope { order('rank DESC') }
 
@@ -29,5 +30,9 @@ class Post < ActiveRecord::Base
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
     end
-  end 
+
+    def create_favorite
+      Favorite.create(post: self, user: self.user)
+      FavoriteMailer.new_post(self).deliver_now
+  end
 end
